ad5f2e9b84eab092bd922406436552cf2f302c31
Kush Jain <mr.kushjain@gmail.com>
2013-06-21 15:50:06 +0530

    seprated code for fetching from view and placed it in model

--- showprofilepage/app/models/profile.rb
+++ showprofilepage/app/models/profile.rb
@@ -1,11 +1,8 @@
 class Profile < ActiveRecord::Base
   attr_accessible :fbprofilehandle, :gender, :name, :speaks
 end
-class Person < ActiveResource::Base
-  self.site = 'http://graph.facebook.com/jainkush/'
-  #self.element_name = "docs"
-
-  def to_json(options={})
-    self.attributes.to_json(options)
-  end
-end
+ require "rubygems" 
+ require "json" 
+ require "open-uri" 
+ string = open("http://graph.facebook.com/jainkush/").read  
+ $parsed = JSON.parse(string) 
