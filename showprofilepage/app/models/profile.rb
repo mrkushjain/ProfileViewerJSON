36785baf79aa1632d025c848a4532d88f2dc4675
Kush Jain <mr.kushjain@gmail.com>
2013-06-21 15:43:03 +0530

    Applied Bootstrap to table and  layout

--- showprofilepage/app/models/profile.rb
+++ showprofilepage/app/models/profile.rb
@@ -1,3 +1,11 @@
 class Profile < ActiveRecord::Base
   attr_accessible :fbprofilehandle, :gender, :name, :speaks
 end
+class Person < ActiveResource::Base
+  self.site = 'http://graph.facebook.com/jainkush/'
+  #self.element_name = "docs"
+
+  def to_json(options={})
+    self.attributes.to_json(options)
+  end
+end
